3edddc45d393dfd980a9bb0b4756abc4cd025c7a
wuzhixuan <627521884@qq.com>
2019-11-27 13:16:28 +0800

    Initial commit

---
+++ src/main/java/com/wuzx/testjenkins/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.wuzx.testjenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/testjenkins")
+    public String testjenkins() {
+        return "hello";
+    }
+}
